d70418129c4870c3565a60b199c6500098590176
ryota <k014c1106@it-neec.jp>
2023-01-04 22:01:31 +0900

    SwiftUI LazyVGrid Smple

---
+++ SwiftUILazyVGridViewDemoApp/Cat.swift
@@ -0,0 +1,14 @@
+//
+//  Cat.swift
+//  SwiftUILazyVGridViewDemoApp
+//
+//  Created by ryota on 2023/01/04.
+//
+
+import Foundation
+
+struct Cat: Identifiable {
+    let id = UUID()
+    let image: String
+    let name:  String
+}
--- SwiftUILazyVGridViewDemoApp/ContentView.swift
+++ SwiftUILazyVGridViewDemoApp/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    let catsList: [Cat] = [
+        Cat(image: "hijiki", name: "ひじき"),
+        Cat(image: "thukushi", name: "つくし"),
+        Cat(image: "hijiki", name: "ひじき"),
+        Cat(image: "thukushi", name: "つくし"),
+        Cat(image: "hijiki", name: "ひじき"),
+        Cat(image: "thukushi", name: "つくし")]
+    
+    let columns: [GridItem] = [GridItem(.adaptive(minimum: 100, maximum: 150))]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(catsList) { cat in
+                    VStack {
+                        Image(cat.image).resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                        Text(cat.name)
+                    }
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
