d41694d760edb95f1b8a74482d5cd5617735ad7e
ryota <k014c1106@it-neec.jp>
2023-01-04 21:56:51 +0900

    Initial Commit

---
+++ SwiftUILazyVGridViewDemoApp/SwiftUILazyVGridViewDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILazyVGridViewDemoAppApp.swift
+//  SwiftUILazyVGridViewDemoApp
+//
+//  Created by ryota on 2023/01/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILazyVGridViewDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
